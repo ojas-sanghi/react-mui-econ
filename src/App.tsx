41d34bb824bdb8620b6078977413ef95074ae17c
Ojas Sanghi <ojas.sanghi@protonmail.com>
2022-03-19 15:23:56 -0700

    Initial commit with beginning workings of react + mui page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import faker from "@faker-js/faker";
+import { Box, Container, AppBar, Toolbar, Typography, Grid, styled, Paper } from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function Chart() {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
+  return <Line data={data} options={options} />;
+}
+
+function TopBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Test Econ React-MUI App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth={false} disableGutters={true}>
+      {TopBar()}
+      {/* <header className="App-header">
+          <title>
+            Me
+          </title>
+        </header> */}
+
+      <Grid container direction="row" justifyContent="center" alignItems="center">
+        <Grid item xs={6}>
+          {Chart()}
+        </Grid>
+        <Grid item xs={6}>
+          {Chart()}
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
